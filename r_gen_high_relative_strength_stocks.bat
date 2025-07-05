dcfde712c01885d50ec30fe8c86a98a526e8a497
陳君豪 <justin.uob@gmail.com>
2025-07-05 11:55:30 +0800

    一鍵選股加註解

--- r_gen_high_relative_strength_stocks.bat
+++ r_gen_high_relative_strength_stocks.bat
@@ -1,12 +1,12 @@
 :: 專門處理 RS>90的強勢股 清單 (約250檔)
-
-:: 從db找出RS>90的強勢股，並把結果存到 high_relative_strength_stocks.txt
 @echo off
 python src\analyze\filter_strong_stocks_by_rs_rsi.py
+:: 從db找出RS>90的強勢股，並把結果存到 high_relative_strength_stocks.txt
 
 @echo off
 python src\fetch\finmind_db_fetcher.py high_relative_strength_stocks.txt
+:: 確保即將篩選的股票數據是最新的
 
 @echo off
 python src\gen_filtered_report_db.py high_relative_strength_stocks.txt
-
+:: 針對RS>90的強勢股進行篩選，生成XQ匯入檔
--- r_gen_shareholding_concentration_list.bat
+++ r_gen_shareholding_concentration_list.bat
@@ -1,7 +1,9 @@
 :: 專門處理 阿信的 籌碼集中度選股 清單 (約150檔)
 @echo off
 python src\fetch\finmind_db_fetcher.py shareholding_concentration_list.txt
+:: 確保即將篩選的股票數據是最新的
 
 @echo off
 python src\gen_filtered_report_db.py
-
+:: 針對RS>90的強勢股進行篩選，生成XQ匯入檔 
+:: 預設是讀取 shareholding_concentration_list.txt，因為這個filter最初是為了 籌碼集中度選股 而設計的
