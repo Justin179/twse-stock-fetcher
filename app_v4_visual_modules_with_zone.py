adeec87d2f9be9576112aa2795cf60a87aeb2497
陳君豪 <justin.uob@gmail.com>
2025-10-26 11:15:11 +0800

    title字變小

--- app_v4_visual_modules_with_zone.py
+++ app_v4_visual_modules_with_zone.py
@@ -34,7 +34,7 @@ plt.rcParams['axes.unicode_minus'] = False
 
 # --- Streamlit ---
 st.set_page_config(page_title="強勢 x 籌碼 x 扣抵", layout="wide")
-st.title("📈 技術線型(找已吃貨且還沒噴的) & 籌碼(守與拉的動機) & 買強勢股")
+st.markdown("#### 📈 技術線型(找已吃貨且還沒噴的) & 籌碼(守與拉的動機) & 買強勢股")
 with st.expander("📘 說明：這是什麼？"):
     st.markdown("""
     - **工作流程: 紅字加碼/鎖利-> 分析匯集精選股-> 高分贏勢股 + 其他**
